aa8f3332f8e42dbfd29ad8e878114a33200326e9
p4cm4n972 <manuel.adele@gmail.com>
2017-01-18 10:09:23 +0100

    boostrap bouton sociaux + lien background

---
+++ javascript.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+var i = 
+    setInterval(function () {
+        document.getElementsByClassName('sprite').contenu.style.left = i + "px";
+    i++
+})
---
+++ js/monscript.js
@@ -0,0 +1,35 @@
+/*$(document).ready(function () {
+var i = 0;
+setInterval(function () {
+$('.progress-bar').attr('style','width:' + i + '%');
+i = i +1;},200);
+});*/
+
+/*$(document).ready(function () {
+    $('.progress-bar').each(function () {
+        $(this).animate({
+            width : $(this).attr("aria-valuenow")+"%"
+        },1000, function () {
+
+        })
+    })
+})*/
+
+/*$(document).ready(function () {
+
+    $('.progress-bar').bind('inview', function (event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            var i = 0;
+            setInterval(function () {
+                $('.progress-bar').attr('style', 'width:' + i + '%');
+                i = i + 1;
+            }, 200);
+        }
+    })
+    })*/
+
+/* $('.progress-bar').each(function (index) {
+                $(this).animate({
+                    width: $(this).delay(1000 * index).attr("aria-valuenow") + "%"
+                }, 1000)*/
+
