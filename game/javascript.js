d3a9aafaf728d7592fa7ab935b61ff1ed9f9ba4d
p4cm4n972 <manuel.adele@gmail.com>
2017-02-07 23:32:21 +0100

    mise en forme GAME creation objet perryPc {}

--- game/javascript.js
+++ game/javascript.js
@@ -1,8 +1,24 @@
 $(document).ready( function deplace() {
-        $('.fond').animate({ left: '-=940'},
-        1000, 'linear',
+        $('.fond').animate({ left: '-=1850'},
+        5000, 'linear',
         function (){
-            $('.fond').css('left',0);
+            $('#fondAnimate1').css('left',0);
             deplace();
         });
+        
+        var perryPc = {
+            posMasqueX: 100,
+            posMasqueY: 400,
+            posSpriteX: 0,
+            posSpriteY: 0,
+            walk : function () {
+                this.posMasqueX = 100;
+                this.posMasqueY = 470;
+                this.posSpriteX = 0;
+                this.posSpriteY = 0;
+                action = function () {
+                    
+                }
+            }
+        }
 })
