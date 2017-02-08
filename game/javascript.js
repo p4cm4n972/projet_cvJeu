5de3bb38582b055dcd11883e8ac56a1174f6fa63
p4cm4n972 <manuel.adele@gmail.com>
2017-02-08 16:28:46 +0100

    GAME creation personnage

--- game/javascript.js
+++ game/javascript.js
@@ -1,32 +1,33 @@
-$(document).ready( function deplace() {
-        $('.fond').animate({ left: '-=1850'},
+$(document).ready(function deplace() {
+    $('.fond').animate({ left: '-=1850' },
         5000, 'linear',
-        function (){
-            $('#fondAnimate1').css('left',0);
+        function () {
+            $('#fondAnimate1').css('left', 0);
             deplace();
         });
+
+     var MonPersonnage = function (x, y, u, v) {
+        this.posSpriteX = x;
+        this.posSpriteY = y;
+        this.posMasqueX = u;
+        this.posMasqueY = v;
+        this.action = function () {
+            $('.masque').css('left', this.posMasqueX + 'px');
+            $('.masque').css('top', this.posMasqueY + 'px');
+            $('.sprite').css('left', this.posSpriteX + 'px');
+            $('.sprite').css('top', this.posSpriteY + 'px');
+        };
+        this.move = function moving () {
+            this.posSpriteX -= 64;
+            requestAnimationFrame(moving);
+        };
         
-        var perryPc = {
-            posMasqueX: 100,
-            posMasqueY: 400,
-            posSpriteX: 0,
-            posSpriteY: 0,
-            walk : function () {
-                this.posMasqueX = '100px';
-                this.posMasqueY = '470px';
-                this.posSpriteX = '0px';
-                this.posSpriteY = '0px';
-                action = function () {
-                    $('.sprite').css('left',this.posSpriteX);
-                    $('.sprite').css('top',this.posSpriteY);
-                    $('.sprite').animate({left:'-=64'},
-                    4000,'linear',
-                    function (){
-                        $('.sprite').css('left',-512);
-                        action();
-                    })
-                }
-            }
-        }
-        perryPc.walk();
+        };
+    var walk = new MonPersonnage(0,0,100,490);
+    var fly = new MonPersonnage(0,-256,100,400);
+    var gun = new MonPersonnage(0,-128,100,490);
+    var death = new MonPersonnage(0,-128,100,490);
+    
+
+    fly.action();
 })
