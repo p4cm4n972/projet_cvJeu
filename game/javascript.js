a2ecf4a797f61eb0e4b5605ab9a3d97d63793eb5
p4cm4n972 <manuel.adele@gmail.com>
2017-02-10 00:19:24 +0100

    GAME mouvement haut bas droite gauche OK

--- game/javascript.js
+++ game/javascript.js
@@ -1,42 +1,71 @@
-$(document).ready( function() {
-     var MonPersonnage = function (x, y, u, v) {
+$(document).ready(function () {
+
+    //TOUCHE PAD DEPLACEMENT DU PERSONNAGE
+    $(document).keydown(function (event) {
+        var upDown = parseInt($('.masque').css('top'));
+        var leftRight = parseInt($('.masque').css('left'));
+        switch (event.which) {
+            case 38:
+                $('.masque').css('top', upDown - 10 + 'px');
+                break;
+            case 40:
+                $('.masque').css('top', upDown + 10 + 'px');
+                break;
+            case 39:
+                $('.masque').css('left', leftRight + 10 + 'px');
+                break;
+            case 37:
+                $('.masque').css('left', leftRight - 10 + 'px');
+                break;
+        }
+    });
+    //ANIMATION FOND
+    var f = 0;
+    var animationFond = function () {
+        $('.fond').css('left', f);
+        f -= 10;
+        if (f == -950) { f = 0 };
+        requestAnimationFrame(animationFond);
+    }
+    animationFond();
+
+
+    //CONSTRUCTEUR DU PERSONNAGE               
+    var MonPersonnage = function (x, y, u, v) {
         this.posSpriteX = x;
         this.posSpriteY = y;
         this.posMasqueX = u;
         this.posMasqueY = v;
         this.action = function () {
-            $('.masque').css('left', this.posMasqueX + 'px');
-            $('.masque').css('top', this.posMasqueY + 'px');
-            $('.sprite').css('left', this.posSpriteX + 'px');
-            $('.sprite').css('top', this.posSpriteY + 'px');
+            $('.masque').css('left', this.posMasqueX);
+            $('.masque').css('top', this.posMasqueY);
+            $('.sprite').css('left', this.posSpriteX);
+            $('.sprite').css('top', this.posSpriteY);
         };
-        
-        };
-    var walk = new MonPersonnage(0,0,100,490);
-    var fly = new MonPersonnage(0,-256,100,400);
-    var gun = new MonPersonnage(0,-128,100,490);
-    var death = new MonPersonnage(0,-128,100,490);
 
-  
-    var f = 0;
-    var animationFond = function (){
-        $('.fond').css('left',f);
-        f -= 10;
-        if (f == -1850) {f = 0};
-        requestAnimationFrame(animationFond);
-    }
-        
-    
-var i = 0;
-var animationPerso = setInterval(function () {
-            $('.sprite').css('left', i );
+    };
+    var walk = new MonPersonnage(0, 0, 100, 490);
+    var fly = new MonPersonnage(0, -256, 100, 400);
+    var gun = new MonPersonnage(0, -128, 100, 490);
+    var death = new MonPersonnage(0, -128, 100, 490);
+
+
+
+    //ANIMATION PERSONNAGE   
+    var i = 0;
+    var animationPerso = setInterval(function () {
+        $('.sprite').css('left', i);
         i -= 64;
-        if (i == -384) {i = 0}
-            }, 3000 / 30);
+        if (i == -384) { i = 0 }
+    }, 4000 / 30);
+
+
+
+
+
+
 
     fly.action();
-    animationFond();
+});
 
-    });
-    
 
